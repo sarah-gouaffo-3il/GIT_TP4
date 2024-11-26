981d7336411d481d71fe50268495ffb6a7894113
sarah-gouaffo-3il <gouaffos@3il.fr>
2024-11-26 17:45:58 +0100

    Ajout de l'exercice1 corrigé

--- exercice_1/exercice1.java
+++ exercice_1/exercice1.java
@@ -1,23 +1,41 @@
+/**
+ * Classe qui répresentant les réductions sur une facture en fonction de la catégorie des produits
+ */
 public class FactureManager {
 
-    // Calcul le total d'une facture
+    /**
+     * @param typeProduit Le type du produit
+     * @param quantite La quantité du produit
+     * @param prixUnitaire Le prix unitaire du produit
+     */
     public double calculerFacture(String typeProduit, int quantite, double prixUnitaire) {
+        /**
+         * Calcul le total d'une facture
+         */ 
         double total = quantite * prixUnitaire;
-
-        // reduction selon la catégorie
-        if (typeProduit.equals("Alimentaire")) {
-            total -= total * 0.05; // Réduction de 5%
-        } else if (typeProduit.equals("Electronique")) {
-            total -= total * 0.1; // Réduction de 10%
-        } else if (typeProduit.equals("Luxe")) {
-            total -= total * 0.15; // Réduction de 15%
-        }
-
-        // Reduction sur le total
-        if (total > 1000) {
+         /**
+         * reduction sur le total
+         */
+         if (total > 1000) {
             total -= total * 0.05; // Réduction supplémentaire de 5% pour les gros montants
         }
 
+        /**
+         * reduction selon la catégorie
+         */ 
+
+        switch (reduction){
+            case "Alimentaire":
+                total -= total * 0.05; // Réduction de 5%
+                break;
+            case "Electronique":
+                total -= total * 0.1; // Réduction de 10%
+                break;
+            case "Luxe":
+                total -= total * 0.15; // Réduction de 15%
+                break;
+            default: total = total;
+        }
         return total;
     }
 }
