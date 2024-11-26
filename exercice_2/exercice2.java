3d816a84a4150bdf27786f940d96c1ccc5da2329
sarah-gouaffo-3il <gouaffos@3il.fr>
2024-11-26 18:06:21 +0100

    Ajout de l'exercice2 corrigé

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,20 +1,36 @@
+/**
+ * Classe représentant la gestion des notes des étudiants
+ */
+
 public class GestionnaireNotes {
 
-    // Affiche note et moyennes
-    public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
-        // Affichage des notes
+    /**
+     * Affiche les notes des étudiants
+     * @param nomEtudiant nom de l'étudiant
+     * @param notes tableau comportant les notes de l'étudiant
+     */
+    public void afficherNotes(String nomEtudiant, int[] notes) {
         System.out.println("Notes de " + nomEtudiant + ":");
         for (int note : notes) {
             System.out.println("- " + note);
         }
+    }
 
-        // Calcul de la somme des notes
+    /*
+    * Calcule la somme des notes 
+    */
+    public int calculerSomme(int[] notes) {
         int somme = 0;
         for (int note : notes) {
             somme += note;
         }
+        return somme;
+    }
 
-        // Affichage de la moyenne
+    /**
+    * Affichage de la moyenne
+    */
+    public void afficherMoyenne(int somme){
         double moyenne = (double) somme / notes.length;
         System.out.println("Moyenne : " + moyenne);
     }
